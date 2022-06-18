d20361376b82ab758ef658755369184d33e3f07c
ivansheshin <ivashesh@gmail.com>
2022-06-18 16:46:50 +0300

    Added todo

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './common.less';
 // const heh = ['1', '1231'];
-
+// TODO Перенести в компонент, подумать над архитектурой
 const API_URL = 'https://api.in.dev-team.club/people?pp=10&p=1';
 
 async function hui() {
