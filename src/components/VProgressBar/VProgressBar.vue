a743ef4c218b7218d92cf8ebf2a58a5cc642d483
ivansheshin <ivashesh@gmail.com>
2022-06-19 17:35:39 +0300

    added progress bar

---
+++ src/components/VProgressBar/VProgressBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="progress">
+    <div class="progress__main"></div>
+    <div class="progress__substrate"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VProgressBar',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    info: {
+      type: Array,
+      required: true,
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+@import './v-progress-bar.less';
+</style>
