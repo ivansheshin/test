488757b7fdaf6d0e22524fbd7175076a0843ab9e
ivansheshin <ivashesh@gmail.com>
2022-06-19 14:59:53 +0300

    added list-component

---
+++ src/components/VList/VList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="`list ${getCssData.list}`">
+    <p 
+      :class="`list__item ${getCssData.item}`"
+      v-for="tagData in tagsData"
+      :key="tagData"
+      :style="`background-color: #${tagData.Color}`"
+    >
+      {{ tagData.Name }}
+    </p>
+  </div> 
+</template>
+
+<script>
+  export default {
+    name: 'VList',
+    props: {
+      mainClass: {
+        type: String,
+          required: true,
+      },
+      tagsData: {
+        type: Array,
+        required: true,
+      }
+    },
+    computed: {
+      getCssData() {
+        return {
+          list:`${this.mainClass}__list`,
+          item: `${this.mainClass}__item`,
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+@import './v-list.less';
+</style>
