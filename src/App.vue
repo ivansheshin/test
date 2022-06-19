7f3614ad0915cbaed0e082eb74415c916330f942
ivansheshin <ivashesh@gmail.com>
2022-06-19 11:50:20 +0300

    Moved get src into app.vue

--- src/App.vue
+++ src/App.vue
@@ -28,13 +28,29 @@ export default {
     data: String,
   },
 
+
+  mounted() {
+    this.getPhotoSrc();
+  },
+
   data() {
     return {
       items: [],
     }
   },
   methods: {
-    
+    async getPhotoSrc() {
+      const API_URL = 'https://api.in.dev-team.club/people?pp=10&p=1';
+
+      await fetch(API_URL)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.items = data;
+        })
+        .catch((error) => console.error(error));
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,31 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './common.less';
-// const heh = ['1', '1231'];
-// TODO Перенести в компонент, подумать над архитектурой
-const API_URL = 'https://api.in.dev-team.club/people?pp=10&p=1';
 
-async function hui() {
-    await fetch(API_URL)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        const app = createApp(App,{
-            data() {
-                return {
-                    data,
-                };
-            }
-        })
-        app.mount('#app');
-    })
-    .catch(() => console.error('Все плохо'))
-;
-}
 
-hui();
-
-// response.json();
-
-// createApp(App).mount('#app')
+createApp(App).mount('#app')
