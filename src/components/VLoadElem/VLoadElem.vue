5748c4d1e38b22096b3bcd949de692911a309cb5
i.sheshin <i.sheshin@tradesoft.ru>
2022-06-20 16:43:29 +0300

    added loader elem

---
+++ src/components/VLoadElem/VLoadElem.vue
@@ -0,0 +1,16 @@
+<template>
+  <span class="load-elem">
+    <span class="load-elem__item"/>
+  </span>
+</template>
+
+<script>
+
+export default {
+  name: 'VLoadElem',
+};
+</script>
+
+<style lang="less" scoped>
+@import './v-load-elem.less';
+</style>
