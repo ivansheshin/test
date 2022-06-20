c973eb0bbf8b1d8688b93804249b333071a82026
i.sheshin <i.sheshin@tradesoft.ru>
2022-06-20 12:50:02 +0300

    added common component for profit and attention

---
+++ src/components/VPersonWorkData/VPersonWorkData.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="person-work-data">
+    <div class="person-work-data__text-content">
+      <p class="person-work-data__title">{{ title }}</p>
+      <p class="person-work-data__text">{{ value }}</p>
+    </div>
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VPersonWorkData',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+@import './v-person-work-data.less';
+</style>
